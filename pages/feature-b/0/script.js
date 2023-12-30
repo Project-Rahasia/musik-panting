f293812e01c5925f351b0ab10d814cf652a538b7
rkhaidir <rkhaidir085@gmail.com>
2023-12-30 23:53:07 +0800

    add feature b

---
+++ pages/feature-b/0/script.js
@@ -0,0 +1,43 @@
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var player;
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('player', {
+    height: '500',
+    width: '100%',
+    videoId: 'gMHsuil3R54',
+    playerVars: {
+      'playsinline': 1
+    },
+    events: {
+      'onStateChange': onPlayerStateChange
+    }
+  });
+}
+
+function onPlayerReady(event) {
+  event.target.playVideo();
+}
+
+var done = false;
+function onPlayerStateChange(event) {
+  if (event.data == YT.PlayerState.PLAYING && !done) {
+    done = true;
+  }
+
+  if (getCurrentTime() == 165.881) {
+    stopVideo()
+  }
+  console.log(getCurrentTime());
+}
+function stopVideo() {
+  player.stopVideo();
+}
+
+function getCurrentTime() {
+  return player.getCurrentTime();
+}
